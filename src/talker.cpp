bafddde24361f883a49e890adb5a754b39c425bb
ltsimps <ltsimpso@me.com>
2017-04-04 15:11:30 -0500

    added the ability to change the frequency of message output on the commandline

--- src/talker.cpp
+++ src/talker.cpp
@@ -23,16 +23,20 @@ std::string newMessage = "FEAR THE TURTLE";
   
   ROS_WARN_STREAM("Now changing output to " << req.input);
 
-  resp.output = req.input;
-  newMessage  = req.input;
-
+  if(req.input ==""){
+  ROS_ERROR_STREAM("There is no string value that you input ");
+  return false;
+  }else {
+    resp.output = req.input;
+    newMessage  = req.input;
+  }
   //return  req.output;
   return true;
 }
 
 
 int main(int argc, char **argv) {
-    ROS_INFO_STREAM("Initializing talker node ");
+    ROS_DEBUG_STREAM("Initializing talker node ");
 
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
@@ -43,8 +47,26 @@ int main(int argc, char **argv) {
     //subscription for listener
     ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
-    // Sets a rate of 10 Hz
-    ros::Rate loop_rate(10);
+   int frequency = 0;  
+    // If argument is passed in change the frequency 
+    if (argc > 1) {
+     ROS_DEBUG_STREAM("argument passed in " << argv[1]);
+     frequency = atoi(argv[1]);
+     
+     if (frequency <=0){
+      ROS_FATAL_STREAM("Incorrect input for frequency ");
+      ROS_WARN_STREAM("Setting value to default hertz value of to ");
+      frequency = 10;
+     }
+    }else{
+    
+    //set to default valueof 10hz
+     frequency = 10;
+    }
+
+   
+    // Sets 
+    ros::Rate loop_rate(frequency);
 
     int count = 0;
     while (ros::ok()) {
