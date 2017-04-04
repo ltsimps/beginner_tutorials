9a89dd47d04092bf2aec89538a4e053826fc0d60
ltsimps <ltsimpso@me.com>
2017-04-04 14:40:19 -0500

    Add a service to change the string in the talker node at will

--- src/talker.cpp
+++ src/talker.cpp
@@ -7,14 +7,40 @@
  */
 
 #include <sstream>
-
+#include <string>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "beginner_tutorials/strings.h"
+
+//std::stringstream change_output(std::string output){
+//} 
+
+std::string newMessage = "FEAR THE TURTLE";
+
+
+ bool change_output(beginner_tutorials::stringsRequest &req,
+                    beginner_tutorials::stringsResponse &resp) {
+  
+  ROS_WARN_STREAM("Now changing output to " << req.input);
+
+  resp.output = req.input;
+  newMessage  = req.input;
+
+  //return  req.output;
+  return true;
+}
+
 
 int main(int argc, char **argv) {
+    ROS_INFO_STREAM("Initializing talker node ");
+
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
-
+    
+    //service to change string output that launches in xterm window 
+    ros::ServiceServer service = n.advertiseService("change_output", change_output);
+    
+    //subscription for listener
     ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
     // Sets a rate of 10 Hz
@@ -25,7 +51,7 @@ int main(int argc, char **argv) {
 
         std_msgs::String msg;
         std::stringstream ss;
-        ss << " FEAR the turtle " << count;
+        ss << newMessage <<" "  << count;
         msg.data = ss.str();
 
         ROS_INFO("%s", msg.data.c_str());
